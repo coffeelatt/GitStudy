166792eba85b4d543cc7a251b67d18c0f40df7b5
coffeelatt <ssongcoffee@gmail.com>
2025-03-20 12:47:37 +0900

    key input

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!!!!!!!!!");
+            Console.Write("이름을 입력해주세요 : ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"입력하신 이름은{name} 입니다.");
+            Console.Write("이대로 생성합니까?")
+                ConsoleKey key = Console.ReadKey(true);
+
+
+
         }
     }
 }
