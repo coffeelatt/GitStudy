3ca25903bf1f3b2d65cd87b7f1729fefb60da64c
coffeelatt <ssongcoffee@gmail.com>
2025-03-20 15:44:50 +0900

    q.1

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -7,11 +7,11 @@
             Console.Write("이름을 입력해주세요 : ");
             string name = Console.ReadLine();
             Console.WriteLine($"입력하신 이름은{name} 입니다.");
-            Console.Write("이대로 생성합니까?")
-                ConsoleKey key = Console.ReadKey(true);
-
-
-
+            Console.Write("이대로 생성합니까?");
+            ConsoleKey key;
+            Console.ReadKey(true);
+            Console.WriteLine("멈추는지확인");
+            
         }
     }
 }
